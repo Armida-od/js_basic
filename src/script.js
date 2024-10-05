98e0da47c53fbb3fb483260ea201dc37d43cf8b4
Anastasiia Ziablova <ziablova25@gmail.com>
2024-10-05 17:22:03 +0300

    HW2: typeOf

--- src/script.js
+++ src/script.js
@@ -1,1 +1,36 @@
-console.log('Hey, this is my first JS program');
+//HW 2 part 1
+
+let hw2101 = 'number' + 23 + 32; // 'number2332' -> string
+let hw2102 = 41 + 1 + 'number'; // '42number' -> string
+let hw2103 = null + 1; // 1 -> number
+let hw2104 = 'five' + + 'two'; // 'fiveNaN' -> string
+let hw2105 = 2 && 7; // 7 -> number
+let hw2106 = +'40' + +'2'; // 42 -> number
+let hw2107 = '10' - 5 === 6; // false -> boolean
+let hw2108 = true + false; // 1 -> number
+let hw2109 = '4px' - 3; // NaN -> number
+let hw2110 = '4' - 3; // 1 -> number
+let hw2111 = '2' + 3 ** 2; // '29' -> string
+let hw2112 = 12 / '6'; // 2 -> number
+let hw2113 = 23 + 42 + 'number'; // '65number' -> string
+let hw2114 = '10' + (5 === 6); // '10false' -> string
+let hw2115 = 'number' + 15 + 3; // 'number153' -> string
+
+//HW 2 part 2
+
+let hw2201 = undefined + 1; // NaN -> number
+let hw2202 = 'true' == true; // false -> boolean
+let hw2203 = false == 'false' // false -> boolean
+let hw2204 = null == '' // false -> boolean
+let hw2205 = 3 ** (9 / 3); // 27 -> number
+let hw2206 = !!'false' == !!'true' // true -> boolean
+let hw2207 = 0 || '0' && 1; // 1 -> number
+let hw2208 = 1 < 2 < 3; // true -> boolean
+let hw2209 = 'foo'+ + +'bar'; // 'fooNaN' -> string
+let hw2210 = 3 ** 2 / 3; // 3 -> number
+let hw2211 = 1 < 2 > 3; // false -> boolean
+let hw2212 = (+null == false) < 1; // false -> boolean
+let hw2213 = false && true || true; // true -> boolean
+let hw2214 = false && (true || true); // false -> boolean
+let hw2215 = (+null == false) < 1 ** 5; // false -> boolean
+
