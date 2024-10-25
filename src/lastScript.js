593cf3db55c39ed6b0f8a95803b0f5b4372d8b40
Anastasiia Ziablova <ziablova25@gmail.com>
2024-10-25 19:44:32 +0300

    HW6: Methods of string

--- src/lastScript.js
+++ src/lastScript.js
@@ -224,3 +224,50 @@ function doubleHolyTree(heightTree) {
     }
 }
 doubleHolyTree(4);
+
+//HW05 task01: Вам необхідно написати функцію reverseString(str), яка приймає на вхід рядок і повертає його у зворотному порядку.
+
+function reverseString(str = 'Ivan'){
+
+    console.log(`Ваша строка ${str}`);
+    let newStr = str.split('', str.length).reverse().join('');
+    console.log(`Ваше слово наоборот будет: ${newStr}`);
+}
+
+reverseString1 = (str = 'Hillel') => str.split('', str.length).reverse().join('');
+
+reverseString();
+reverseString('Anastasiia');
+console.log('_____________________\n');
+//HW05 task02: Вам необхідно написати функцію isPalindrome(str), яка приймає на вхід рядок і перевіряє, чи є введений рядок паліндромом.
+
+function isPalindrome(str = 'level'){
+
+    console.log(`Ваша строка "${str}"`);
+    let onlySymbolStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    let reverseStr = onlySymbolStr.split('', str.length).reverse().join('');
+
+    onlySymbolStr === reverseStr
+        ? console.log(`Cтрока "${str}" является палидромом`)
+        : console.log(`Cтрока "${str}" не является палидромом`);
+
+}
+isPalindrome();
+isPalindrome('Madam, I\'m Adam');
+isPalindrome('Hillel school');
+
+//HW05 task03:
+//Вам необхідно написати функцію findGCD(a, b), яка приймає на вхід два числа і повертає їхній НСД.
+console.log('_____________________\n');
+function findGCD(numberOne, numberTwo) {
+    console.log(`Ваши два числа: первое число = ${numberOne}, второе число = ${numberTwo}`);
+
+    for (; numberTwo !== 0;) {
+        let gcd = numberTwo;
+        numberTwo = numberOne % numberTwo;
+        numberOne = gcd;
+    }
+    return console.log(`Общий делитель для ваших чисел будет равен ${numberOne}`);
+}
+
+findGCD(20, 50);
--- src/script.js
+++ src/script.js
@@ -1,44 +1,33 @@
-//HW05 task01: Вам необхідно написати функцію reverseString(str), яка приймає на вхід рядок і повертає його у зворотному порядку.
+//HW06 task01
+//Вам необхідно написати функцію doubleLetter(str),
+//яка приймає на вхід рядок і повертає новий рядок, у якому кожен символ повторюється двічі hello ⇒ hheelllloo.
 
-function reverseString(str = 'Ivan'){
+const doubleLetter = (str = 'hello') => str.replaceAll(/(.)/g, '$1$1');
+console.log(doubleLetter());
 
-    console.log(`Ваша строка ${str}`);
-    let newStr = str.split('', str.length).reverse().join('');
-    console.log(`Ваше слово наоборот будет: ${newStr}`);
+//HW06 task02
+//Вам необхідно написати функцію padString(str, length, symbol, toLeft), яка приймає на вхід рядок, число, що є довгим рядком,
+//який ми хочемо отримати в результаті та символ, яким доповниться рядок, якщо це буде потрібно, четвертим параметром є буремний «прапор»,
+//чи додавати символи зліва або справа(за замовчуванням).
+//Якщо 2 параметр менше, ніж довжина вихідного рядка, то виводимо вихідний рядок без змін. Приклад виклику: padString('Ivan', 6, '*') // 'Ivan**'.
+
+
+function padString(str = 'Ivan', length = 6, symbol = '*', toLeft = false) {
+    if (length <= str.length)
+    return str;
+    return toLeft ? str.padStart(length, symbol) : str.padEnd(length, symbol);
 }
 
-reverseString();
-reverseString('Anastasiia');
-console.log('_____________________\n');
-//HW05 task02: Вам необхідно написати функцію isPalindrome(str), яка приймає на вхід рядок і перевіряє, чи є введений рядок паліндромом.
+console.log(padString());
+console.log(padString("Hillel", 10, '!', true));
 
-function isPalindrome(str = 'level'){
 
-    console.log(`Ваша строка "${str}"`);
-    let onlySymbolStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-    let reverseStr = onlySymbolStr.split('', str.length).reverse().join('');
+//HW06 task03
+//Вам необхідно написати функцію camelCase(str, separator), яка приймає на вхід рядок і перетворює його до формату camelCase.
 
-    onlySymbolStr === reverseStr
-    ? console.log(`Cтрока "${str}" является палидромом`)
-    : console.log(`Cтрока "${str}" не является палидромом`);
+const camelCase = (str = 'Hillel School', separator = ' ') =>
+    str.charAt(0).toLowerCase() + str.slice(1).replaceAll(separator, '');
 
-}
-isPalindrome();
-isPalindrome('Madam, I\'m Adam');
-isPalindrome('Hillel school');
-
-//HW05 task03:
-//Вам необхідно написати функцію findGCD(a, b), яка приймає на вхід два числа і повертає їхній НСД.
-console.log('_____________________\n');
-function findGCD(numberOne, numberTwo) {
-    console.log(`Ваши два числа: первое число = ${numberOne}, второе число = ${numberTwo}`);
-
-    for (; numberTwo !== 0;) {
-        let gcd = numberTwo;
-        numberTwo = numberOne % numberTwo;
-        numberOne = gcd;
-    }
-    return console.log(`Общий делитель для ваших чисел будет равен ${numberOne}`);
-}
-
-findGCD(20, 50);
+console.log(camelCase());
+console.log(camelCase('Earth-Ukraine-Odessa-Deribasovskaya', '-'));
+console.log(camelCase('big.bang.theory', '.'));
