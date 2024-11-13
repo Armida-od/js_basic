189ed85006b57e67f15d2e104a13dd6358fc2c71
Anastasiia Ziablova <ziablova25@gmail.com>
2024-11-13 19:41:08 +0200

    HW12: tasks 1-3

--- src/lastScript.js
+++ src/lastScript.js
@@ -534,3 +534,79 @@ function calculateCircle(radius) {
 const memoizedCircle = memoize(calculateCircle);
 console.log(memoizedCircle(30));
 
+
+console.log("HW11 task01")
+//Вам необхідно написати функцію-декоратор logArguments(fn), яка приймає на вхід функцію і додає можливість логувати всі аргументи, передані у функцію-аргумент.
+
+function logArguments(fn){
+    return function(...args) {
+        console.log(`Функция ${fn.name} принимает параметры:`, args);
+        return fn(...args);
+    };
+}
+
+//Применение функции logArguments для вычисления функции calculateVolumeParallelepiped
+const calculateVolumeParallelepiped = (height, width, length) => height * width * length;
+
+const loggedCalculateCircle = logArguments(calculateVolumeParallelepiped);
+loggedCalculateCircle(20, 30, 50);
+
+
+console.log("--------------  \nHW11 task02")
+//Вам необхідно написати функцію-декоратор validate(fn, validator), яка приймає на вхід функцію і додає можливість перевіряти аргументи,
+//передані у функцію fn, на відповідність заданому validator. Якщо аргументи не проходять перевірку, то декоратор має викидати виняток.
+
+function validate(fn, validator) {
+    return function(...args) {
+        if (!validator(...args)) {
+            throw new Error('Аргументы не являются числами.');
+        }
+        return fn(...args);
+    };
+}
+
+const isAllNumbers = (...args) => args.every(arg => typeof arg === 'number');
+
+//Применение валидации функции calculateVolumeCylinder
+const calculateVolumeCylinder = (radius, height) => Math.PI * Math.pow(radius, 2) * height;
+
+const validated = validate(calculateVolumeCylinder, isAllNumbers);
+
+try {
+    console.log(validated(2, 3));
+    console.log(validated('15', '2'));
+} catch (error) {
+    console.error(error.message);
+}
+
+console.log("-------------- \nHW11 task03")
+//Вам необхідно написати функцію-декоратор retry(fn, maxAttempts), яка приймає на вхід функцію
+//і додає можливість викликати функцію з максимальною кількістю спроб у разі помилки та повертає результат останнього виклику.
+function retry(fn, maxAttempts) {
+    return function(...args) {
+        for (let item = 1; item <= maxAttempts; item++) {
+            try {
+                const result = fn(...args);
+                console.log(`Число ${result} находится в диапазоне от 1 до 10`);
+                return result;
+            } catch (error) {
+                console.error(`Попытка ${item} не удалась:`, error);
+            }
+        }
+        throw new Error(`Исчерпаны все попытки ${maxAttempts} `);
+    };
+}
+
+function checkNumber(num) {
+    if (num < 1 || num > 10) {
+        throw new Error(`Число ${num} не находится в диапазоне от 1 до 10`);
+    }
+    return num;
+}
+
+const retryCheckNumber = retry(checkNumber, 3);
+
+retryCheckNumber(5);
+retryCheckNumber(51);
+
+//Для себя!!! '^' указывает на строку, где мы явно генерируем исключение, а не на те строки, где происходили предыдущие ошибки.
--- src/script.js
+++ src/script.js
@@ -1,75 +1,63 @@
-console.log("HW11 task01")
-//Вам необхідно написати функцію-декоратор logArguments(fn), яка приймає на вхід функцію і додає можливість логувати всі аргументи, передані у функцію-аргумент.
+console.log("HW12 task01")
+//Вам необхідно написати функцію summarize(num), яка приймає на вхід число і повертає функцію, яка під час виклику додає це число до аргументу
+//і повертає результат. Якщо аргумент не передано, то додається одиниця.
+//Наприклад, якщо функція викликається з аргументом 5, то функція, що повертається,
+//повинна при виклику з аргументом 3 повернути 8 (тому що 3 + 5 = 8) або 6, якщо аргумент не буде передано.
 
-function logArguments(fn){
-    return function(...args) {
-        console.log(`Функция ${fn.name} принимает параметры:`, args);
-        return fn(...args);
+function Summarizer(num) {
+    this.num = num;
+    this.sum = function(item = 1) {
+        return this.num + item;
     };
 }
 
-//Применение функции logArguments для вычисления функции calculateVolumeParallelepiped
-const calculateVolumeParallelepiped = (height, width, length) => height * width * length;
-
-const loggedCalculateCircle = logArguments(calculateVolumeParallelepiped);
-loggedCalculateCircle(20, 30, 50);
-
+const result = new Summarizer(5);
+console.log(result.sum(3));
+console.log(result.sum());
 
 console.log("--------------  \nHW11 task02")
-//Вам необхідно написати функцію-декоратор validate(fn, validator), яка приймає на вхід функцію і додає можливість перевіряти аргументи,
-//передані у функцію fn, на відповідність заданому validator. Якщо аргументи не проходять перевірку, то декоратор має викидати виняток.
+//Вам необхідно написати функцію counter(startValue, step), яка приймає на вхід два параметри - стартове значення лічильника і його крок.
+//Функція повертає нову функцію, яка при кожному виклику збільшує лічильник на значення і повертає його поточне значення.
+//Лічильник повинен мати методи increment, decrement і reset, які збільшують або зменшують значення на step і скидають значення до стартового, відповідно.
 
-function validate(fn, validator) {
-    return function(...args) {
-        if (!validator(...args)) {
-            throw new Error('Аргументы не являются числами.');
+function counter(startValue, step){
+    return {
+        startValue,
+        step,
+        increment(){
+            return startValue += step;
+        },
+        decrement(){
+            return startValue -= step;
+        },
+        reset(){
+            return startValue;
         }
-        return fn(...args);
-    };
+    }
 }
 
-const isAllNumbers = (...args) => args.every(arg => typeof arg === 'number');
+const generalCounter = counter(15, 5);
 
-//Применение валидации функции calculateVolumeCylinder
-const calculateVolumeCylinder = (radius, height) => Math.PI * Math.pow(radius, 2) * height;
-
-const validated = validate(calculateVolumeCylinder, isAllNumbers);
-
-try {
-    console.log(validated(2, 3));
-    console.log(validated('15', '2'));
-} catch (error) {
-    console.error(error.message);
-}
+console.log(`Початкове значення лічильника: ${generalCounter.startValue}`);
+console.log(`Лічильник збільшився на ${generalCounter.step} та став: ${generalCounter.increment()}`);
+console.log(`Потім лічильник зменшився на ${generalCounter.step} та став: ${generalCounter.decrement()}`);
+console.log(`Після скидання лічильника його значення повернулось до початкового: ${generalCounter.reset()}`);
 
 console.log("-------------- \nHW11 task03")
-//Вам необхідно написати функцію-декоратор retry(fn, maxAttempts), яка приймає на вхід функцію
-//і додає можливість викликати функцію з максимальною кількістю спроб у разі помилки та повертає результат останнього виклику.
-function retry(fn, maxAttempts) {
-    return function(...args) {
-        for (let item = 1; item <= maxAttempts; item++) {
-            try {
-                const result = fn(...args);
-                console.log(`Число ${result} находится в диапазоне от 1 до 10`);
-                return result;
-            } catch (error) {
-                console.error(`Попытка ${item} не удалась:`, error);
-            }
-        }
-        throw new Error(`Исчерпаны все попытки ${maxAttempts} `);
-    };
+//Вам необхідно написати функцію sequence(fn, fn), яка приймає на вхід дві або більше функції й повертає нову функцію, яка викликає їх послідовно з результатом попереднього виклику. Результат останньої функції має бути повернутий новою функцією.
+//Кожна функція повинна мати доступ до результату попередньої функції через замикання.
+
+// 1. sequence(...fns) принимает любое количество функций и возвращает новую функцию.
+// 2. Новая функция вызывает входящие функции последовательно.
+// 3. Результат каждой функции будет передаваться как аргумент следующий функции (через замыкание) и уже результат последней функции будет результатом выполнения нашей новой созданной функции.
+
+function sequence(...fns) {
+    return (...args) => fns.reduce((result, fn) => fn(result), ...args);
 }
 
-function checkNumber(num) {
-    if (num < 1 || num > 10) {
-        throw new Error(`Число ${num} не находится в диапазоне от 1 до 10`);
-    }
-    return num;
-}
+const yearlySalary = (salary) => salary * 12;
+const salaryCoefficient = (salary) => salary * 3;
+const tax = (salary) => salary - ((salary * 5)/100);
 
-const retryCheckNumber = retry(checkNumber, 3);
-
-retryCheckNumber(5);
-retryCheckNumber(51);
-
-//Для себя!!! '^' указывает на строку, где мы явно генерируем исключение, а не на те строки, где происходили предыдущие ошибки.
+const totalSalary = sequence(yearlySalary, salaryCoefficient, tax);
+console.log(`Total salary is ${totalSalary(1000)}`);
